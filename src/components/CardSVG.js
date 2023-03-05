6cd9243bac9f17fdf84cab0946943db244c40ad5
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2023-03-05 20:52:00 +0330

    add card svg

---
+++ src/components/CardSVG.js
@@ -0,0 +1,33 @@
+const styles = StyleSheet.create({
+    btn: {
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        backgroundColor: '#0893FC',
+        padding: 10,
+        borderRadius: 5,
+    },
+    image: {
+        width: 400,
+        height: 461,
+    },
+    text: {
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: '500',
+    },
+    title: {
+        fontSize: 50,
+        fontWeight: '400',
+        marginVertical: 30,
+        textAlign: 'center',
+    },
+    description: {
+        fontSize: 18,
+        color: 'gray',
+        textAlign: 'center',
+        marginHorizontal: 20,
+        marginBottom: 50,
+    }
+})
